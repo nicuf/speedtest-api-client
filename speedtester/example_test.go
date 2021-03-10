391aa53d22fae200ab6c7aef0294c04bc9486012
Nicolae Furtuna <nicolae.furtuna@telenav.com>
2021-03-10 20:39:04 +0200

    Add example for NewSpeedTester.

--- speedtester/example_test.go
+++ speedtester/example_test.go
@@ -15,3 +15,17 @@ func ExampleGetSpeeds() {
 	}
 	fmt.Printf("fast.com results: download: %.2f Mbps, upload: %.2f Mbps\n", download, upload)
 }
+
+func ExampleNewSpeedTester() {
+	tester, err := NewSpeedTester(FastCom)
+	if err != nil {
+		fmt.Println("Got an error durring creation of a speed tester: ", err)
+	}
+
+	err = tester.Run()
+	if err != nil {
+		fmt.Println("Got an error when runing the speed tests: ", err)
+	}
+
+	fmt.Printf("Download: %.2fMbps, Upload: %.2fMbps\n", tester.GetDownloadSpeed(), tester.GetUploadSpeed())
+}
